822ff7f81bd4df6d9ea81f29769563c6f3eb5648
Pablo Altamirano <pabloaltamirano.a@gmail.com>
2020-10-31 22:13:13 -0600

    added main database controller with 4 methods

---
+++ Database/examples.php
@@ -0,0 +1,23 @@
+<?php
+
+    include_once "Database.php";
+
+    $objectReadTable = new Database("localhost", "root", "gym");
+
+    #read method
+    #$result ='';
+    #$result = $objectReadTable -> read("users", array('id', 'usuario', 'pass'));
+
+    #var_dump($result[0]['usuario']);
+    
+    #create method
+    #$result = $objectReadTable -> create('users', array('usuario', 'pass'), array('usuarioPHP', 'desdePHP'));
+    
+    #update method
+    #$result = $objectReadTable -> update('users',array('usuario','pass'),array('Arce','pastrami'),'usuario', 'Altamirano')
+
+    #delete method
+    #$result = $objectReadTable -> delete('users', 'usuario', 'usuarioPHP')
+
+
+?>
