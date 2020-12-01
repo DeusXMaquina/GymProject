b50722f3515036b3c29cbc841141388c20724785
reginamt03 <regina.madero.torres@gmail.com>
2020-12-01 10:26:06 -0600

    adding updateDeleteInvoice.php

---
+++ Payments/pages/updateDeleteInvoice.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Slots Template</title>
+  <link rel="stylesheet" href="/GymProject/Admin-Slots/css/normalize.css">
+  <link rel="stylesheet" href="/GymProject/Admin-Slots/css/mainTemplate.css" />
+  <link rel='stylesheet' href='/GymProject/Admin-Slots/css/courses/editremovecourses.css'>
+</head>
+
+<body>
+  <header class="site-header">
+    <nav class="navigation">
+      <a href="#">
+        <img style="height: 80px; width: 80px;" src="/GymProject/pictures/cedefiOf.png" alt="cedefiLogo" />
+      </a>
+      <a href="/GymProject/Admin-Slots/pages/mainSlots/mainSlots.html">Home</a>
+      <div class='dropdown'>
+        <a class='dropbtn'>Courses</a>
+        <div class='dropdown-content'>
+            <a href='/GymProject/Admin-Slots/pages/Courses/addCourses.php'>Add Course</a>
+            <a href='/GymProject/Admin-Slots/pages/Courses/editRemoveCourses.php'>Edit/Remove Course</a>
+            <a href='/GymProject/Admin-Slots/pages/Courses/viewCourses.php'>View Courses</a>
+        </div>
+      </div>
+      <a href="">Time Slots</a>
+      <a href="">Support</a></li>
+    </nav>
+  </header>
+
+  <!--Title-->
+  <h1 style='text-align:center'>Edit or Remove Courses</h1>
+
+  <!--Table class-->
+  <form method='post'>
+  <table id='invoiceTable'>
+    <tr>
+      <th>InvoiceNumber</th>
+      <th>User</th>
+      <th>Total Payed</th>
+      <th>Date Issued</th>
+    </tr>
+    <?php
+      include_once '/xampp/htdocs/GymProject/Database/Database.php';
+
+      $objectReadTable = new Database('localhost', 'root', 'gym');
+      $userPaymentsResults = $objectReadTable -> read('userpaymentdata');
+      $paymentsResults = $objectReadTable -> read('payments');
+      $userResults = $objectReadTable -> read('users');
+
+      function findUserName ($userArray, $userPaymentsArray,$id) {
+        for ($index = 0; $index < count($userPaymentsArray); $index++) {
+          if ($userPaymentsArray[$index]['id'] === $id){
+            return $userArray[$index]['nombre'];
+          }
+        }
+        return 'No Instructor Assigned';
+      }
+
+      function insertTableData ($array, $user, $paymentsUser, $id) {
+        $userFlag = 0;
+        echo '<tr>';
+        foreach ($array as $val) {
+          if($userFlag === 1){
+            echo '<td id="user">'.findUserName($user,$paymentsUser,$val).'</td>';
+            $userFlag++;
+          } else {
+            echo '<td>'.$val.'</td>';
+            $userFlag++;
+          }
+         }
+         echo '<td><button id='.$id.' class="removeUpdateButton" type="submit" formaction="controllerUpdateCourse.php">Update</button><button id='.$id.' class="removeUpdateButton" type="submit" formaction="controllerDeleteCourse.php">Remove</button></td></tr>';
+       }
+       for ($indexRow = 0; $indexRow < count($paymentsResults); $indexRow ++) {
+         insertTableData($paymentsResults[$indexRow], $userResults, $userPaymentsResults,$paymentsResults[$indexRow]['idUserPaymentData']);
+       }
+    ?>
+  </table>
+</form>
+  {+}
+        <a href="">Time Slots</a>
+        <a href="">Support</a></li>
+      </nav>
+      <img style="height: 50px; width: 50px;" src="/GymProject/pictures/cedefiOf.png" alt="cedefiLogo" />
+      <p class="copyright">Copyright &copy; Universidad del Valle de Atemajac </p>
+    </div>
+  </footer>
+</body>
+<script src='editRemoveScript.js'></script>
+</html>
